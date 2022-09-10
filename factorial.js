365100c3bb199855f66b6deb97cfb1d9ca98b65f
uddipon15-13523 <uddipon15-13523@diu.edu.bd>
2022-09-10 09:56:54 +0600

    odd even numbrr check

--- factorial.js
+++ factorial.js
@@ -1,11 +1,11 @@
 //fromula n!=n*(n-1)*(n-2)*...*1
-function fact(n){
-    let result =1;
-    let i =n;
-    while(i>=1){
-        result =result*i;
+function paul(n) {
+    let result = 1;
+    let i = n;
+    while (i >= 1) {
+        result = result * i;
         i--;
     }
     return result;
 }
-console.log(fact(5));
+console.log(paul(5));
---
+++ oddEvenNumber.js
@@ -0,0 +1,12 @@
+function oddEvenNumber (arr){
+    for(let i =0; i<arr.length; i++){
+      if(arr[i]%2==0){
+        console.log(arr[i]);
+      }
+      else{
+        //console.log(arr[i]);
+      }
+    }
+}
+const arr = [1,2,3,4,5,6,7,8,9];
+oddEvenNumber(arr);
